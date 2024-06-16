a6af343275855b108baa515802763dd29792f130
Patrick Raedler <readpato@Patos-MBP.fritz.box>
2024-06-16 16:15:38 +0200

    chore: enable root eslint

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,3 +1,18 @@
+local util = require "lspconfig.util"
+local root_file = {
+  ".eslintrc",
+  ".eslintrc.js",
+  ".eslintrc.cjs",
+  ".eslintrc.yaml",
+  ".eslintrc.yml",
+  ".eslintrc.json",
+  "eslint.config.js",
+  "eslint.config.mjs",
+  "eslint.config.cjs",
+  "eslint.config.ts",
+  "eslint.config.mts",
+  "eslint.config.cts",
+}
 ---@type LazySpec
 return {
   "AstroNvim/astrolsp",
@@ -21,6 +36,10 @@ return {
     ---@diagnostic disable: missing-fields
     config = {
       eslint = {
+        root_dir = function(fname)
+          root_file = util.insert_package_json(root_file, "eslintConfig", fname)
+          return util.root_pattern(unpack(root_file))(fname)
+        end,
         on_attach = function(client, bufnr)
           vim.api.nvim_create_autocmd("BufWritePre", {
             buffer = bufnr,
